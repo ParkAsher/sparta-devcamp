ff3708fccc2b9aa3b84359fc7ae0ae3ddb33050c
ParkAsher <devasherpark@naver.com>
2024-03-21 00:31:01 +0900

    [feat]: Typeorm 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggingInterceptor } from './interceptors';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             envFilePath: `.env`,
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get<string>('DB_HOST'),
+                port: configService.get<number>('DB_PORT'),
+                username: configService.get<string>('DB_USERNAME'),
+                password: configService.get<string>('DB_PASSWORD'),
+                database: configService.get<string>('DB_DATABASE'),
+                autoLoadEntities: true, // 엔티티들이 자동으로 불려와짐
+                synchronize: configService.get<string>('RUNTIME') !== 'prod', // 배포환경이 아니면 true
+                logging: configService.get<string>('RUNTIME') !== 'prod',
+            }),
         }),
     ],
     controllers: [AppController],
